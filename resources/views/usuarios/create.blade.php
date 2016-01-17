775f60b8ff1e6c78c02523915de4187fca4c5efa
estebangramosf <esteban.ramos.f@gmail.com>
2016-01-17 00:28:20 -0300

    Respaldo -> Ajuste de diseño en registro

--- resources/views/usuarios/create.blade.php
+++ resources/views/usuarios/create.blade.php
@@ -3,13 +3,15 @@
 <div class="jumbotron">
 	<div id="contentIn">
 		@include('alerts.alertFields')
-		<h4>Registro de clientes</h4>
-
-		{!!Form::open(['route'=>'usuarios.store', 'method'=>'POST'])!!}
-			@include('usuarios.forms.fieldsUser')
-			{!!Form::submit('Registrar', ['class'=>'btn btn-primary btn-success'])!!}
-		{!!Form::close()!!}
-		
+		<div class="panel panel-default">
+			<div class="panel-heading"><h4>Registro de clientes</h4></div>
+			<div class="panel-body">
+				{!!Form::open(['route'=>'usuarios.store', 'method'=>'POST'])!!}
+					@include('usuarios.forms.fieldsUser')
+					{!!Form::submit('Registrar', ['class'=>'btn btn-primary btn-success'])!!}
+				{!!Form::close()!!}
+			</div>
+		</div>		
 	</div>
 </div>
 @stop
