5cbbf11af3adfd5321cee8e7d7aa6724d146ce97
estebangramosf <esteban.ramos.f@gmail.com>
2016-01-26 16:16:20 -0300

    recibiendo

--- resources/views/empresas/index.blade.php
+++ resources/views/empresas/index.blade.php
@@ -3,29 +3,37 @@
 <div class="jumbotron">
 	<div id="contentIn">
 		@include('alerts.alertFields')
+		@include('alerts.errorsMessage')
 		@include('alerts.successMessage')
-		<table class="table">
-			<thead>
-				<th>Nombre</th>
-				<th>Correo</th>
-				<th>Ciudad</th>
-				<th>Fono</th>
-				<th>Aniversario Empresa</th>
-				<th>Encargado</th>
-				<th>Operaciones</th>
-			</thead>
-			@foreach($empresas as $empresa)	
-			<tbody>
-				<td>{{$empresa->nombre}}</td>
-				<td>{{$empresa->email}}</td>
-				<td>{{$empresa->ciudad}}</td>
-				<td>{{$empresa->fono}}</td>
-				<td>{{$empresa->fecha_creacion}}</td>
-				<td>{{$empresa->nombre_encargado}}</td>
-				<td>{!!link_to_route('empresas.edit', $title = 'Editar', $parameters = $empresa->id, $attributes = ['class'=>'btn btn-primary'])!!}</td>
-			</tbody>
-			@endforeach
-		</table>	
+		@include('alerts.warningMessage')
+		<h2>Panel de administración</h2>		
+		<div class="panel panel-default">
+			<div class="panel-heading"><h4>Mantenedor de empresas</h4></div>
+			<div class="panel-body">
+				<table class="table">
+					<thead>
+						<th>Nombre</th>
+						<th>Correo</th>
+						<th>Ciudad</th>
+						<th>Fono</th>
+						<th>Aniversario Empresa</th>
+						<th>Encargado</th>
+						<th>Operaciones</th>
+					</thead>
+					@foreach($empresas as $empresa)	
+					<tbody>
+						<td>{{$empresa->nombre}}</td>
+						<td>{{$empresa->email}}</td>
+						<td>{{$empresa->ciudad}}</td>
+						<td>{{$empresa->fono}}</td>
+						<td>{{$empresa->fecha_creacion}}</td>
+						<td>{{$empresa->nombre_encargado}}</td>
+						<td>{!!link_to_route('empresas.edit', $title = 'Editar', $parameters = $empresa->id, $attributes = ['class'=>'btn btn-primary'])!!}</td>
+					</tbody>
+					@endforeach
+				</table>	
+			</div>
+		</div>	
 		{!!$empresas->render()!!}
 	</div>
 </div>
--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -3,29 +3,37 @@
 <div class="jumbotron">
 	<div id="contentIn">
 		@include('alerts.alertFields')
+		@include('alerts.errorsMessage')
 		@include('alerts.successMessage')
-		<table class="table">
-			<thead>
-				<th>Nombre</th>
-				<th>Correo</th>
-				<th>Ciudad</th>
-				<th>Fono</th>
-				<th>Cumplea&ntilde;os</th>
-				<th>Sexo</th>
-				<th>Operaciones</th>
-			</thead>
-			@foreach($users as $user)	
-			<tbody>
-				<td>{{$user->nombre}}</td>
-				<td>{{$user->email}}</td>
-				<td>{{$user->ciudad}}</td>
-				<td>{{$user->fono}}</td>
-				<td>{{$user->fecha_nacimiento}}</td>
-				<td>{{$user->sexo}}</td>
-				<td>{!!link_to_route('usuarios.edit', $title = 'Editar', $parameters = $user->id, $attributes = ['class'=>'btn btn-primary'])!!}</td>
-			</tbody>
-			@endforeach
-		</table>	
+		@include('alerts.warningMessage')
+		<h2>Panel de administración</h2>		
+		<div class="panel panel-default">
+			<div class="panel-heading"><h4>Mantenedor de usuarios</h4></div>
+			<div class="panel-body">
+				<table class="table">
+					<thead>
+						<th>Nombre</th>
+						<th>Correo</th>
+						<th>Ciudad</th>
+						<th>Fono</th>
+						<th>Cumplea&ntilde;os</th>
+						<th>Sexo</th>
+						<th>Operaciones</th>
+					</thead>
+					@foreach($users as $user)	
+					<tbody>
+						<td>{{$user->nombre}}</td>
+						<td>{{$user->email}}</td>
+						<td>{{$user->ciudad}}</td>
+						<td>{{$user->fono}}</td>
+						<td>{{$user->fecha_nacimiento}}</td>
+						<td>{{$user->sexo}}</td>
+						<td>{!!link_to_route('usuarios.edit', $title = 'Editar', $parameters = $user->id, $attributes = ['class'=>'btn btn-primary'])!!}</td>
+					</tbody>
+					@endforeach
+				</table>	
+			</div>
+		</div>
 		{!!$users->render()!!}
 	</div>
 </div>
