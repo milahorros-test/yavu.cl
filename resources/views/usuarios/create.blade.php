add87dd63f39b9cc102b29e68126d52c45732fd5
estebangramosf <esteban.ramos.f@gmail.com>
2016-02-04 10:06:55 -0300

    Respaldo dia anterior

--- resources/views/usuarios/create.blade.php
+++ resources/views/usuarios/create.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.front')
 @section('content')
 <div class="jumbotron">
-	<div id="contentIn">
+	<div id="contentMiddle">
 		@include('alerts.alertFields')
 		<h4>Registro de cliente </h4>
-		<div class="panel panel-default">
-			<div class="panel-heading"><h4>Registro de clientes</h4></div>
-			<div class="panel-body">
+		<div class="row">
+
 				{!!Form::open(['route'=>'usuarios.store', 'method'=>'POST'])!!}
 				@include('usuarios.forms.fieldsUser')
 				<div class="form-group has-feedback has-feedback-left col-sm-6">							
 					{!!Form::label('Registrar')!!}						
 					{!!Form::submit('Registrar', ['class'=>'btn btn-primary btn-success col-sm-12'])!!}
 					{!!Form::close()!!}
+					
 				</div>	
 			</div>
-		</div>		
+		</div>			
 	</div>
 </div>
 @stop
--- resources/views/usuarios/forms/fieldsUser.blade.php
+++ resources/views/usuarios/forms/fieldsUser.blade.php
@@ -1,147 +1,161 @@
+<div class="col-sm-8">
+	<div class="list-group" >
+		<div class="list-group-item">
+			<h4>Datos principales</h4>
+		</div>
+		<div class="list-group-item">
+			<div class="form-group has-feedback has-feedback-left">
+				{!!Form::label('Nombre:')!!}
+				{!!Form::text('nombre',null,['class'=>'form-control','placeholder'=>'Ingrese su nombre'])!!}
+			</div>
 
-<div class="form-group has-feedback has-feedback-left col-sm-6">
-	{!!Form::label('Nombre:')!!}
-	{!!Form::text('nombre',null,['class'=>'form-control','placeholder'=>'Ingrese su nombre'])!!}
+			<div class="form-group has-feedback has-feedback-left">
+				{!!Form::label('Apellido:')!!}
+				{!!Form::text('apellido',null,['class'=>'form-control','placeholder'=>'Ingrese su apellido'])!!}
+			</div>
+
+			<div class="form-group has-feedback has-feedback-left">
+				{!!Form::label('Email:')!!}
+				{!!Form::email('email',null,['class'=>'form-control','placeholder'=>'Ingrese su email'])!!}			
+			</div>
+			<div class="form-group has-feedback has-feedback-left">
+				@if(Auth::user()->check())
+					{!!Form::label('Cambio de clave:')!!}		
+				@else
+					{!!Form::label('Nueva clave:')!!}
+				@endif
+				{!!Form::password('password',['class'=>'form-control','placeholder'=>'Ingrese una clave'])!!}		
+			</div>
+
+			<div class="form-group has-feedback has-feedback-left">
+				{!!Form::label('Ciudad:')!!}		
+				{!!Form::select('ciudad', 
+					['Tarapacá' => 'Tarapacá',
+					'Parinacota' => 'Parinacota',
+					'Arica' => 'Arica',
+					'Antofagasta' => 'Antofagasta',
+					'Atacama' => 'Atacama',
+					'La Serena' => 'La Serena', 
+					'Coquimbo' => 'Coquimbo', 
+					'Valparaiso' => 'Valparaiso',
+					'Aconcagua' => 'Aconcagua',
+					'Región Metropolitana' => 'Región Metropolitana',
+					'O Higgins' => 'O Higgins',
+					'Curicó' => 'Curicó',
+					'Talca' => 'Talca',
+					'Linares' => 'Linares',
+					'Maule' => 'Maule',
+					'Ñuble' => 'Ñuble',
+					'Concepción' => 'Concepción',
+					'Arauco' => 'Arauco',
+					'Biobío' => 'Biobío',	
+					'Malleco' => 'Malleco',
+					'Cautín' => 'Cautín',
+					'Araucanía' => 'Araucanía',
+					'Los Ríos' => 'Los Ríos',
+					'Valdivia' => 'Valdivia',
+					'Osorno' => 'Osorno',
+					'Los Lagos' => 'Los Lagos',
+					'Llanquihue' => 'Llanquihue',	
+					'Chiloé' => 'Chiloé',	
+					'Aysen' => 'Aysen',	
+					'Magallanes' => 'Magallanes',	
+					'otra' => 'otras...'], 
+					$selected = null, ['class' => 'form-control']) 
+				!!}			
+			</div>	
+		</div>
+	</div>
 </div>
-
-<div class="form-group has-feedback has-feedback-left col-sm-6">
-	{!!Form::label('Apellido:')!!}
-	{!!Form::text('apellido',null,['class'=>'form-control','placeholder'=>'Ingrese su apellido'])!!}
-</div>
-
-<div class="form-group has-feedback has-feedback-left col-sm-6">
-	{!!Form::label('Email:')!!}
-	{!!Form::email('email',null,['class'=>'form-control','placeholder'=>'Ingrese su email'])!!}			
-</div>
-<div class="form-group has-feedback has-feedback-left col-sm-6">
-	@if(Auth::user()->check())
-		{!!Form::label('Cambio de clave:')!!}		
-	@else
-		{!!Form::label('Nueva clave:')!!}
-	@endif
-
-	{!!Form::password('password',['class'=>'form-control','placeholder'=>'Ingrese una clave'])!!}		
-</div>
-
-<div class="form-group has-feedback has-feedback-left col-sm-6">
-	{!!Form::label('Ciudad:')!!}		
-	{!!Form::select('ciudad', 
-		['Tarapacá' => 'Tarapacá',
-		'Parinacota' => 'Parinacota',
-		'Arica' => 'Arica',
-		'Antofagasta' => 'Antofagasta',
-		'Atacama' => 'Atacama',
-		'La Serena' => 'La Serena', 
-		'Coquimbo' => 'Coquimbo', 
-		'Valparaiso' => 'Valparaiso',
-		'Aconcagua' => 'Aconcagua',
-		'Región Metropolitana' => 'Región Metropolitana',
-		'O Higgins' => 'O Higgins',
-		'Curicó' => 'Curicó',
-		'Talca' => 'Talca',
-		'Linares' => 'Linares',
-		'Maule' => 'Maule',
-		'Ñuble' => 'Ñuble',
-		'Concepción' => 'Concepción',
-		'Arauco' => 'Arauco',
-		'Biobío' => 'Biobío',	
-		'Malleco' => 'Malleco',
-		'Cautín' => 'Cautín',
-		'Araucanía' => 'Araucanía',
-		'Los Ríos' => 'Los Ríos',
-		'Valdivia' => 'Valdivia',
-		'Osorno' => 'Osorno',
-		'Los Lagos' => 'Los Lagos',
-		'Llanquihue' => 'Llanquihue',	
-		'Chiloé' => 'Chiloé',	
-		'Aysen' => 'Aysen',	
-		'Magallanes' => 'Magallanes',	
-		'otra' => 'otras...'], 
-		$selected = null, ['class' => 'form-control']) 
-	!!}			
-</div>	
 
 @if(Auth::user()->check())
+	<div class="col-sm-4">
+		<div class="list-group" >
+			<div class="list-group-item">
+				<h4>Datos de Contacto</h4>
+			</div>
+			<div class="list-group-item">
+				<div class="form-group has-feedback has-feedback-left">
+					{!!Form::label('Rut:')!!}
+					{!!Form::text('rut',null,['class'=>'form-control','placeholder'=>'Ingrese su rut'])!!}
+				</div>
 
-	<div class="form-group has-feedback has-feedback-left col-sm-6">
-		{!!Form::label('Rut:')!!}
-		{!!Form::text('rut',null,['class'=>'form-control','placeholder'=>'Ingrese su rut'])!!}
+				<div class="form-group has-feedback has-feedback-left">
+					{!!Form::label('Nickname:')!!}
+					{!!Form::text('login',null,['class'=>'form-control','placeholder'=>'Ingrese su nombre de usuario'])!!}			
+				</div>
+
+				<div class="form-group has-feedback has-feedback-left">
+					{!!Form::label('Dirección:')!!}
+					{!!Form::text('direccion',null,['class'=>'form-control','placeholder'=>'Ingrese dirección'])!!}
+				</div>
+
+				<div class="form-group has-feedback has-feedback-left">
+					{!!Form::label('Región:')!!}
+					{!!Form::select('region', 
+						['I Región' => 'I Región',
+						'II Región' => 'II Región',	
+						'III Región' => 'III Región',	
+						'IV Región' => 'IV Región',
+						'V Región' => 'V Región',
+						'VI Región' => 'VI Región',
+						'VII Región' => 'VII Región',
+						'VIII Región' => 'VIII Región',
+						'IX Región' => 'IX Región',
+						'X Región' => 'X Región',
+						'XI Región' => 'XI Región',
+						'XII Región' => 'XII Región',
+						'XIII Región' => 'XIII Región',
+						'XVI Región' => 'XVI Región',
+						'XV Región' => 'XV Región'],
+						$selected = null, ['class' => 'form-control']) 
+					!!}			
+				</div>
+
+				<div class="form-group has-feedback has-feedback-left">
+					{!!Form::label('País:')!!}
+					{!!Form::select('pais', 
+						['Argentina' => 'Argentina',
+						'Bolivia' => 'Bolivia',	
+						'Brasil' => 'Brasil',	
+						'Colombia' => 'Colombia',	
+						'Chile' => 'Chile',	
+						'Ecuador' => 'Ecuador',	
+						'Mexico' => 'Mexico',	
+						'Panama' => 'Panama',	
+						'Paraguay' => 'Paraguay',	
+						'Venezuela' => 'Venezuela',	
+						'Uruguay' => 'Uruguay',	
+						'Otro' => 'Otro'], 
+						$selected = null, ['class' => 'form-control']) 
+					!!}	
+				</div>
+
+				<div class="form-group has-feedback has-feedback-left">
+					{!!Form::label('Teléfono Movil:')!!}
+					{!!Form::text('fono',null,['class'=>'form-control','placeholder'=>''])!!}
+				</div>
+
+				<div class="form-group has-feedback has-feedback-left">
+					{!!Form::label('Teléfono Fijo:')!!}
+					{!!Form::text('fono_2',null,['class'=>'form-control','placeholder'=>''])!!}
+				</div>
+
+				<div class="form-group has-feedback has-feedback-left">
+					{!!Form::label('Sexo:')!!}
+					{!!Form::select('sexo', 
+						['Masculino' => 'Masculino',
+						'Femenino' => 'Femenino',	
+						'Otro' => 'Otro'], 
+						$selected = null, ['class' => 'form-control']) 
+					!!}		
+				</div>
+
+				<div class="form-group has-feedback has-feedback-left">
+					{!!Form::label('Cumpleaños:')!!}
+					{!!Form::date('fecha_nacimiento',null,['class'=>'form-control','placeholder'=>''])!!}
+				</div>
+			</div>
+		</div>
 	</div>
-
-	<div class="form-group has-feedback has-feedback-left col-sm-6">
-		{!!Form::label('Nickname:')!!}
-		{!!Form::text('login',null,['class'=>'form-control','placeholder'=>'Ingrese su nombre de usuario'])!!}			
-	</div>
-
-	<div class="form-group has-feedback has-feedback-left col-sm-6">
-		{!!Form::label('Dirección:')!!}
-		{!!Form::text('direccion',null,['class'=>'form-control','placeholder'=>'Ingrese dirección'])!!}
-	</div>
-
-	<div class="form-group has-feedback has-feedback-left col-sm-6">
-		{!!Form::label('Región:')!!}
-		{!!Form::select('region', 
-			['I Región' => 'I Región',
-			'II Región' => 'II Región',	
-			'III Región' => 'III Región',	
-			'IV Región' => 'IV Región',
-			'V Región' => 'V Región',
-			'VI Región' => 'VI Región',
-			'VII Región' => 'VII Región',
-			'VIII Región' => 'VIII Región',
-			'IX Región' => 'IX Región',
-			'X Región' => 'X Región',
-			'XI Región' => 'XI Región',
-			'XII Región' => 'XII Región',
-			'XIII Región' => 'XIII Región',
-			'XVI Región' => 'XVI Región',
-			'XV Región' => 'XV Región'],
-			$selected = null, ['class' => 'form-control']) 
-		!!}			
-	</div>
-
-	<div class="form-group has-feedback has-feedback-left col-sm-6">
-		{!!Form::label('País:')!!}
-		{!!Form::select('pais', 
-			['Argentina' => 'Argentina',
-			'Bolivia' => 'Bolivia',	
-			'Brasil' => 'Brasil',	
-			'Colombia' => 'Colombia',	
-			'Chile' => 'Chile',	
-			'Ecuador' => 'Ecuador',	
-			'Mexico' => 'Mexico',	
-			'Panama' => 'Panama',	
-			'Paraguay' => 'Paraguay',	
-			'Venezuela' => 'Venezuela',	
-			'Uruguay' => 'Uruguay',	
-			'Otro' => 'Otro'], 
-			$selected = null, ['class' => 'form-control']) 
-		!!}	
-	</div>
-
-	<div class="form-group has-feedback has-feedback-left col-sm-6">
-		{!!Form::label('Teléfono Movil:')!!}
-		{!!Form::text('fono',null,['class'=>'form-control','placeholder'=>''])!!}
-	</div>
-
-	<div class="form-group has-feedback has-feedback-left col-sm-6">
-		{!!Form::label('Teléfono Fijo:')!!}
-		{!!Form::text('fono_2',null,['class'=>'form-control','placeholder'=>''])!!}
-	</div>
-
-	<div class="form-group has-feedback has-feedback-left col-sm-6">
-		{!!Form::label('Sexo:')!!}
-		{!!Form::select('sexo', 
-			['Masculino' => 'Masculino',
-			'Femenino' => 'Femenino',	
-			'Otro' => 'Otro'], 
-			$selected = null, ['class' => 'form-control']) 
-		!!}		
-	</div>
-
-	<div class="form-group has-feedback has-feedback-left col-sm-6">
-		{!!Form::label('Cumpleaños:')!!}
-		{!!Form::date('fecha_nacimiento',null,['class'=>'form-control','placeholder'=>''])!!}
-	</div>
-
 @endif
