a7cb0dcb325f9c8a9e2c2885ee4c67aba93ccd52
estebangramosf <esteban.ramos.f@gmail.com>
2016-02-10 10:02:39 -0300

    Formato moneda para coins

--- public/js/ajax/GestionarCoins.js
+++ public/js/ajax/GestionarCoins.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){	
 	/*DECLARACIÓN DE VARIABLES GLOBALES*/
+	var formatNumber = {
+	 separador: ".", // separador para los miles
+	 sepDecimal: ',', // separador para los decimales
+	 formatear:function (num){
+	  num +='';
+	  var splitStr = num.split('.');
+	  var splitLeft = splitStr[0];
+	  var splitRight = splitStr.length > 1 ? this.sepDecimal + splitStr[1] : '';
+	  var regx = /(\d+)(\d{3})/;
+	  while (regx.test(splitLeft)) {
+	  splitLeft = splitLeft.replace(regx, '$1' + this.separador + '$2');
+	  }
+	  return this.simbol + splitLeft  +splitRight;
+	 },
+	 new:function(num, simbol){
+	  this.simbol = simbol ||'';
+	  return this.formatear(num);
+	 }
+	}	
 	/*DECLARACIÓN DE VARIABLES GLOBALES*/
 
 	/*MÉTODOS CONSTRUCTORES*/
@@ -20,7 +39,7 @@ $(document).ready(function(){
 			$(res).each(function(key,value){
 				console.log(value.coins);
 				if(parseInt(value.coins)>0){
-					$("#CantidadCoins").append(value.coins);	
+					$("#CantidadCoins").append(formatNumber.new(value.coins, "$ "));	
 				}
 					//$("#CantidadCoins").html("<p>0</p>");	
 			});
@@ -56,6 +75,7 @@ $(document).ready(function(){
 			});
 		});	
 	}
+
 
 	/*FUNCIONES Y PROCEDIMIENTOS*/
 
