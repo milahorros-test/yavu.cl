704219219da1547a2fbfd88d1ac7539a873b3c6d
estebangramosf <esteban.ramos.f@gmail.com>
2016-02-10 16:42:13 -0300

    sos

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,16 +9,16 @@
     <div class="panel panel-default">
       <div class="panel-heading">Login</div>
         <div class="panel-body">
-          <div class="form-group has-feedback has-feedback-left col-md-12">
+          <div class="form-group has-feedback has-feedback-left ">
             {!!Form::label('Usuario:')!!}
             {!!Form::email('email',null,['class'=>'form-control','placeholder'=>'Ingresa tu email de usuario'])!!}     
           </div>
-          <div class="form-group has-feedback has-feedback-left col-md-12">
+          <div class="form-group has-feedback has-feedback-left ">
             {!!Form::label('Clave:')!!}
             {!!Form::password('password',['class'=>'form-control','placeholder'=>'Ingresa tu clave'])!!}     
           </div>
-          <div class="form-group has-feedback has-feedback-left col-md-12">
-            {!!Form::submit('Acceder',['class'=>'btn btn-default col-md-12'])!!}
+          <div class="form-group has-feedback has-feedback-left ">
+            {!!Form::submit('Acceder',['class'=>'btn btn-default ', 'style'=>'width:100%;'])!!}
           </div>
         </div>
     </div>
