d83120e4a1b459ab7018cb537c085fd4a4d68033
estebangramosf <esteban.ramos.f@gmail.com>
2016-02-10 14:50:38 -0300

    Respaldo

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'mysql.hostinger.es'),
-            'database'  => env('DB_DATABASE', 'u420456370_yavu'),
-            'username'  => env('DB_USERNAME', 'u420456370_root'),
-            'password'  => env('DB_PASSWORD', 'milahorros2016'),
+            'host'      => env('DB_HOST', '127.0.0.1'),
+            'database'  => env('DB_DATABASE', 'yavu'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', 'root'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
