b0622f33394655c25039a933b0f20cc383115d09
estebangramosf <esteban.ramos.f@gmail.com>
2016-02-19 09:50:50 -0300

    Notificaciones -> Pop {modelo, controlador}

---
+++ database/migrations/2016_02_19_094944_create_pops_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pops', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pops');
+    }
+}
