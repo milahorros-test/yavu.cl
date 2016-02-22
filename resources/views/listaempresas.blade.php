d232240c74a07bdc2b48ac15b3e41e7e3516ddaf
Panic919 <luis.zepeda@milahorros.cl>
2016-02-22 12:18:31 -0300

    Cambio en el buscador de listaempresas

--- resources/views/listaempresas.blade.php
+++ resources/views/listaempresas.blade.php
@@ -1,46 +1,38 @@
-@extends('layouts.front')	
+{!!Html::script('js/jquery.js')!!}
+{!!Html::script('js/ajax/BuscarEmpresa.js')!!}
+@extends('layouts.front')
 @section('content')
 <div class="jumbotron">
-	<div id="contentIn">
+	<div id="contentMiddle">
 		@include('alerts.alertFields')
 		@include('alerts.errorsMessage')
 		@include('alerts.successMessage')
 		@include('alerts.warningMessage')
-			
+		<h2>Panel de administración</h2>		
 		<div class="panel panel-default">
-			<div class="panel-heading"><h4>Empresas Asociadas!</h4></div>
+			<div class="panel-heading"><h4>Mantenedor de empresas</h4></div>
 			<div class="panel-body">
-				<table class="table">
+				<form class="">
+					<div class="form-group">
+						{!!Form::text('nombre',null,['class'=>'form-control','placeholder'=>'buscar...','id'=>'empresa'])!!}
+					</div>
+					<a href="#!" class="btn btn-primary btn-sm" id="BuscarEmpresa">Buscar</a>
+				</form>
+				<table id="EmpresaList" class="table table-hover">
+					<thead>
+						<th>Nombre</th>
 
-					
-					{!!Form::open(['action' => 'EmpresaController@ListaEmpresas', 'method' => 'GET', 'class' => 'navbar-form navbar-left pull-right', 'role' => 'search' ])!!}
-  						<div class="form-group">
-  								
-   					{!!Form::text('nombre',null,['class' => 'form-control', 'placeholder' => 'Nombre de empresa',])!!}
-   					
+					</thead>
+					@foreach($empresas as $empresa)	
+					<tbody>
+						<td>{{$empresa->nombre}}</td>
 
-  						</div>
- 						 <button type="submit" class="btn btn-default">Buscar</button>				
-
-				@foreach($empresas as $empresa)	
-
-				  <div class="container" id="tourpackages-carousel">
-				      <div class="row">				        
-				        <div class="col-md-6">
-				          <div class="thumbnail">
-				            <img src="{!!URL::to('images/empresa.png')!!}" alt="">
-				              <div class="caption">
-				                <h4>{{$empresa->nombre}}</h4>
-				            </div>
-				            <h4>Descripcion empresa</h4>
-				            <h4>Mas info</h4>
-				          </div>
-				        </div>
-
-				@endforeach
+						
+					</tbody>
+					@endforeach
 				</table>	
 			</div>
-		</div>	
+		</div>
 		{!!$empresas->render()!!}
 	</div>
 </div>
