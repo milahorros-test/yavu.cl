b99f7c6a4eaa508ee1979f998eaccc48587b6a58
Panic919 <luis.zepeda@milahorros.cl>
2016-02-24 13:40:20 -0300

    CRUD Banner ->  Controller, Model, Request 6

--- resources/views/banners/forms/fieldsBanner.blade.php
+++ resources/views/banners/forms/fieldsBanner.blade.php
@@ -9,28 +9,34 @@
 				{!!Form::text('banner_empresa',null,['class'=>'form-control','placeholder'=>'Ingrese el nombre del banner'])!!}
 			</div>
 
-			<div class="form-group has-feedback has-feedback-left">
-				{!!Form::label('Descripción:')!!}
-				{!!Form::textarea('descripcion',null,['class'=>'form-control','maxlength'=>'500','placeholder'=>'Ingrese la descripcion del sorteo'])!!}
-      </div>
-
 
         <div class="form-group has-feedback has-feedback-left">
-          {!!Form::label('Fecha de inicio Sorteo:')!!}
-          {!!Form::date('fecha_inicio_sorteo',null,['class'=>'form-control','placeholder'=>''])!!}
+          {!!Form::label('Ingrese Enlace de la Empresa:')!!}
+          {!!Form::text('enlace_empresa',null,['class'=>'form-control','placeholder'=>''])!!}
 
         </div> 
 
+         <div class="form-group has-feedback has-feedback-left">
+          {!!Form::label('Ingrese imagen empresa:')!!}
+          {!!Form::text('imagen_empresa',null,['class'=>'form-control','placeholder'=>''])!!}
+
+        </div> 
+
+        <div class="form-group has-feedback has-feedback-left">
+				{!!Form::label('Descripción a publicitar:')!!}
+				{!!Form::textarea('comentario_banner',null,['class'=>'form-control','maxlength'=>'500','placeholder'=>'Ingrese la descripcion del sorteo'])!!}
+      </div>
+
         	 <div class="form-group has-feedback has-feedback-left">
-            {!!Form::hidden('estado_sorteo', 'Pendiente')!!} 
+            {!!Form::hidden('estado_banner', 'Pendiente')!!} 
           </div>             
        
   		
          		@if (Auth::admin()->check()) 
 
 			<div class="form-group has-feedback has-feedback-left">
-				{!!Form::label('Estado Sorteo:')!!}		
-				{!!Form::select('estado_sorteo', 
+				{!!Form::label('Estado Banner:')!!}		
+				{!!Form::select('estado_banner', 
 					['Pendiente' => 'Pendiente',	
 					'Aprobado' => 'Aprobado'], 
 					$selected = null, ['class' => 'form-control']) 
@@ -44,9 +50,3 @@
 		</div>
     </div>
         </div>		
-<!--</div>-->
-
-<!-- $table->string('banner_empresa', 200)->unique();
-            $table->string('enlace_empresa', 100);
-            $table->string('imagen_empresa', 100);   
-            $table->string('comentario_banner', 100);  
