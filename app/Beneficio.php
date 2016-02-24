491f17363d661748efddf6a9129762f315c55463
estebangramosf <esteban.ramos.f@gmail.com>
2016-02-24 15:01:09 -0300

    Modelos, controladores para pago y beneficios

---
+++ app/Beneficio.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace yavu;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Beneficio extends Model
+{
+    //
+}
---
+++ database/migrations/2016_02_24_145949_create_beneficios_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBeneficiosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beneficios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('beneficios');
+    }
+}
