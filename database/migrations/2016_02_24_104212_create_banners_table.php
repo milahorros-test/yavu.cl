ed6dc96003257c8a0021706ae0da4db011c531a6
Panic919 <luis.zepeda@milahorros.cl>
2016-02-24 11:21:14 -0300

    CRUD Banner ->  Controller, Model, Request 2

---
+++ database/migrations/2016_02_24_104212_create_banners_table.php
@@ -0,0 +1,23 @@
+<?php
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+class CreateBannersTable extends Migration
+{
+    public function up()
+    {
+        Schema::dropIfExists('banners');
+        Schema::create('banners', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('banner_empresa', 200)->unique();
+            $table->string('enlace_empresa', 100);
+            $table->string('imagen_empresa', 100);   
+            $table->string('comentario_banner', 100);          
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+    public function down()
+    {
+        Schema::drop('banners');
+    }
+}
